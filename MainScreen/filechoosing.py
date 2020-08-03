c1867b9af2983e17409c1df034a24826691b29ca
Chadial <marcus-schulz@hotmail.de>
2020-08-03 14:04:52 +0200

    Learning FileChooser from kivy p2

--- MainScreen/filechoosing.py
+++ MainScreen/filechoosing.py
@@ -2,25 +2,33 @@
 https://www.geeksforgeeks.org/python-file-chooser-in-kivy/
 """
 
-import kivy
+# Program to explain how to use File chooser in kivy
 
+# import kivy module
+import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+kivy.require('1.9.0')
 
-
+# create the layout class
 class Filechooser(BoxLayout):
     def select(self, *args):
         try:
-            self.input.text = args[1][0]
+            self.xhit.text = args[1][0]
+            print(args[0])
+            print(args[0][1])
+            print(args[1])
         except:
             pass
 
 
+# Create the App class
 class FileApp(App):
     def build(self):
         return Filechooser()
 
+    # run the App
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     FileApp().run()
-
